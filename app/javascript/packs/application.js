90ff4eaaba7262b8f295eaa411e6fb0cdc91fdad
hitoshi.hasegawa <hitoshi_hasegawa@acceluniverse.com>
2021-11-14 22:07:48 +0100

    プロフィールページの実装

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("src/profile_image_upload");
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -17,3 +18,4 @@ require("channels")
 
 import 'bootstrap';
 import '../stylesheets/application.scss';
+import '@fortawesome/fontawesome-free/js/all';
