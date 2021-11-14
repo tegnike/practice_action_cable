bdee4f55254b3ffb4c43fc5e2b1afaaac94000c3
hitoshi.hasegawa <hitoshi_hasegawa@acceluniverse.com>
2021-11-14 22:56:32 +0100

    画像のスワイプ機能の実装

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,7 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require("src/profile_image_upload");
+require("src/swipe");
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -17,5 +18,6 @@ require("src/profile_image_upload");
 // const imagePath = (name) => images(name, true)
 
 import 'bootstrap';
+import 'hammerjs';
 import '../stylesheets/application.scss';
 import '@fortawesome/fontawesome-free/js/all';
